2d2c36aabe1eae7b399c6cfc50ff331ef4324f66
Igor <igor.filipiuk@gmail.com>
2019-02-21 13:25:13 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArchiveComponent } from './archive/archive.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomeComponent},
+// {path: 'archive', component: ArchiveComponent},
+// {path: 'archive/:year,', component: ArchiveComponent},
+{ path: 'archive/:year/:month', component: ArchiveComponent},
+{ path: '**', component: NotFoundComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
