365944c5bfe6beda917e50018106d8f2d59b06e8
Igor <igor.filipiuk@gmail.com>
2019-02-21 01:48:04 +0100

    added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ArchiveComponent } from './archive/archive.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
-    ArchiveComponent
+    ArchiveComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
