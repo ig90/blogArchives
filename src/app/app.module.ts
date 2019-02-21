9c1eed946c04bd18b904473c76dec83a7e23f79e
Igor <igor.filipiuk@gmail.com>
2019-02-21 01:21:14 +0100

    added archive component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ArchiveComponent } from './archive/archive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    ArchiveComponent
   ],
   imports: [
     BrowserModule,
